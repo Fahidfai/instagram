5cb81cdccd92c3f74413a3fc6fa20f92b9723024
Fahidfai <fahidfai33@gmail.com>
2026-04-21 01:17:33 +0400

    Update instagram.sh

--- instagram.sh
+++ instagram.sh
@@ -9,7 +9,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 YELLOW='\033[1;33m'
-NC='\033[0m' 
+NC='\033[0mbsbbzb' 
 
 show_banner() {
     clear
@@ -39,7 +39,9 @@ start_audit() {
             sleep 0.5
             
             # Simple simulation logic
-            if [[ "$password" == "123456" || "$password" == "password" ]]; then
+            if [[ "$password" == "1234
+            
+    +971567867086|| "$password" == "password" ]]; then
                 echo -e "${RED}[VULNERABLE]${NC}"
             else
                 echo -e "${GREEN}[SECURE]${NC}"
